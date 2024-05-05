98749325af6ce8388847921fe2e9351fbd3b9858
Marie Bednarova <48547424+xbedna72@users.noreply.github.com>
2024-05-05 19:07:19 +0200

    Update AnalyseUpdateStats.py
    Update

--- AnalyseUpdateStats.py
+++ AnalyseUpdateStats.py
@@ -1,5 +1,3 @@
-import os
-
 def ParseLine(_line:str) -> list[(int, str)]:
     parsedLine = _line.split('|')
     output = []
@@ -13,14 +11,11 @@ def ParseLine(_line:str) -> list[(int, str)]:
     return output
 
 class AnalyseProperties:
-    updates = 0
-    depths = []
-    modifiedNodes = 0
-    addressNotFound = 0
-    action_updates_array = []
-
     def __init__(self):
         self.action_updates_array = [0]
+        self.updates = 0
+        self.depths = []
+        self.modifiedNodes = 0
 
 class UpdatesAnalyser:
     properties = []       #id 0 = save, id 1 = delete
@@ -28,18 +23,24 @@ class UpdatesAnalyser:
     def __init__(self):
         self.properties.append(AnalyseProperties())
         self.properties.append(AnalyseProperties())
+        self.levels = []
     
     def SaveAction(self, line):
-        self._Action(line, 0)    
+        self._Action(line, 0)
 
     def DeleteAction(self, line):
-        self._Action(line, 1)
+        if len(line) > 0:
+            self._Action(line, 1)
 
     def _Action(self, line, id):
         action = ParseLine(line)
-        self.properties[id].updates = self.properties[id].updates + 1
-        self.properties[id].depths.append(len(action))
         
+        self.properties[id].updates += 1
+        if id == 1:
+            self.properties[id].depths.append(len(action)-1)
+        else:
+            self.properties[id].depths.append(len(action))
+    
         for step in action:
             if step[1] == True:
                 self.AddAction(step[0], id)
@@ -49,56 +50,57 @@ class UpdatesAnalyser:
             for i in range(0, (level + 1) - len(self.properties[id].action_updates_array)):
                 self.properties[id].action_updates_array.append(0)
 
-        self.properties[id].modifiedNodes = self.properties[id].modifiedNodes + 1
-        self.properties[id].action_updates_array[level] = self.properties[id].action_updates_array[level] + 1
+        self.properties[id].modifiedNodes += 1
+        self.properties[id].action_updates_array[level] += 1
+
+        if len(self.levels) < (level+1):
+            for i in range(0, (level + 1) - len(self.levels)):
+                self.levels.append([0])
+        
+        val = self.levels[level][-1]
+        if id == 0:
+            self.levels[level].append(val+1)
+        else:
+            self.levels[level].append(val-1)
 
     def FinishAnalyse(self, _file: str):
-        fileHandle = open(_file, "w")
+        fileHandle = open(_file, "+w")
 
-        fileHandle.write("SAVE UPDATES ::\n")
-        actionUpdates = self.properties[0].updates  
-        for i in range(0, len(self.properties[0].action_updates_array)):
-            fileHandle.write(f"level {i}: {self.properties[0].action_updates_array[i]} ({((self.properties[0].action_updates_array[i]/actionUpdates) *100):2.2f}%)\n")
-        fileHandle.write(f"created nodes: {self.properties[0].modifiedNodes}\n")
-        fileHandle.write(f"all updates: {self.properties[0].updates}\n")
-        fileHandle.write(f"created nodes per action update: {self.properties[0].modifiedNodes/actionUpdates}\n")
-        allDepth = 0
-        max = 0
-        min = len(self.properties[0].action_updates_array)
+        fileHandle.write("-----------------------------------------------------------------------------\n")
+        fileHandle.write("     |    SAVE UPDATES    |    DELETE UPDATES    |   Total number of nodes   \n")
+        fileHandle.write("-----------------------------------------------------------------------------\n")
+        deleteUpdates = self.properties[1].updates
+        saveUpdates = self.properties[0].updates
+        fileHandle.write("Level|    Updates      %  |      Updates      %  |        Min       Max\n")
+        
+        insert_list_length = len(self.properties[0].action_updates_array)
+        delete_list_length = len(self.properties[1].action_updates_array)
+        _ar = insert_list_length if insert_list_length > delete_list_length else delete_list_length
+        for i in range(0, _ar):
+            line=""
+            
+            line+=f"{i:3}. |"
 
-        for d in self.properties[0].depths:
-            allDepth = allDepth + d
-            if max < d:
-                max = d
-            if min > d:
-                min = d
+            if i < insert_list_length:
+                line+=f"{self.properties[0].action_updates_array[i]:7}"
+                line+=f"{((self.properties[0].action_updates_array[i]/saveUpdates) *100):10.0f}% |"
+            else:
+                line+=f"                      |"
 
-        fileHandle.write(f"averageDepth: {allDepth/self.properties[0].modifiedNodes}\n")
-        fileHandle.write(f"min depth: {min}\n")
-        fileHandle.write(f"max depth: {max}\n")
+            if i < delete_list_length:
+                line+=f"{self.properties[1].action_updates_array[i]:10}"
+                line+=f"{((self.properties[1].action_updates_array[i]/deleteUpdates) *100):10.0f}% |"
+            else:
+                line+=f"                      |"
 
-        fileHandle.write("\n")
-        fileHandle.write("DELETE UPDATES ::\n")
-        actionUpdates = self.properties[1].updates
-        for i in range(0,len(self.properties[1].action_updates_array)):
-            fileHandle.write(f"level {i}: {self.properties[1].action_updates_array[i]} ({((self.properties[1].action_updates_array[i]/actionUpdates) *100):2.2f}%)\n")
-        fileHandle.write(f"deleted nodes: {self.properties[1].modifiedNodes}\n")
-        fileHandle.write(f"all updates: {self.properties[1].updates}\n")
-        fileHandle.write(f"deleted nodes per action update: {self.properties[1].modifiedNodes/actionUpdates}\n")
-        allDepth = 0
-        max = 0
-        min = len(self.properties[1].action_updates_array)
+            line+=f"{min(self.levels[i]):10}{max(self.levels[i]):10}\n"
+            fileHandle.write(line)
+                    
+        fileHandle.write("-----------------------------------------------------------------------------\n")
+        fileHandle.write(f"     Number of updates:{saveUpdates:3}|Number of updates:{deleteUpdates:3} |\n")
+        fileHandle.write(f"     Created nodes:{self.properties[0].modifiedNodes:3}    |Removed nodes:{self.properties[1].modifiedNodes:3}\n")
 
-        for d in self.properties[1].depths:
-            allDepth = allDepth + d
-            if max < d:
-                max = d
-            if min > d:
-                min = d
-
-        fileHandle.write(f"averageDepth: {allDepth/self.properties[1].modifiedNodes}\n")
-        fileHandle.write(f"min depth: {min}\n")
-        fileHandle.write(f"max depth: {max}\n")
-
+        fileHandle.write(f"     min depth:{min(self.properties[0].depths):3}        |min depth:{min(self.properties[1].depths):3}\n")
+        fileHandle.write(f"     max depth:{max(self.properties[0].depths):3}        |max depth:{max(self.properties[1].depths):3}\n")
 
         fileHandle.close()
